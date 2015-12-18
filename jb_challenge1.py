9ca18af3a2c3a00eb398fc756be3d60c12dfef00
binkocd <binkocd@gmail.com>
2015-12-17 23:30:46 -0800

    Add args, dict, change print.

--- jb_challenge1.py
+++ jb_challenge1.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python
+import argparse
 
 file = open("story.txt", "r")
 file_contents = file.read()
+
+stats = {}
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Processes file statistics.')
+    parser.add_argument('-f', '--story-file', dest='story_file', default='story.txt', help='Story file that you want the stats for.')
+    args = parser.parse_args()
+    print stats
+    #for result in log_scan(args.log_directory, args.log_file_format, args.log_date, args.log_date_format):
+        #print result
 
 
 def get_words(story):
     '''Count number of words using split()'''
     words = len(story.split())
+    stats["Words"] += 1
+    print stats["Words"]
     return words
 
 
@@ -38,7 +52,11 @@ def get_unique_count(story):
             pass
     return wordcount
 
-print "Total Unique Words: {}".format(get_unique_count(file_contents))
-print "Total Word Count: {}".format(get_words(file_contents))
-print "Paragraphs: {}".format(get_paragraphs(file_contents))
-print "Sentences: {}".format(get_sentences(file_contents))
+#print "Total Unique Words: {}".format(get_unique_count(file_contents))
+#print "Total Word Count: {}".format(get_words(file_contents))
+#print "Paragraphs: {}".format(get_paragraphs(file_contents))
+#print "Sentences: {}".format(get_sentences(file_contents))
+
+
+if __name__ == "__main__":
+    main()
